2ce4e84811378bec56b066ec7320c34b5b6ab23f
clxrityy <clxrityxyz@gmail.com>
2025-10-01 15:30:31 -0400

    use context factory

--- apps/demo/app/page.tsx
+++ apps/demo/app/page.tsx
@@ -1,12 +1,30 @@
-import { useExampleHook } from "@enotion/hooks";
+"use client";
+import { JSX } from "react";
 import styles from "./page.module.css";
+import { useContextFactory } from "@enotion/hooks";
 
-export default function Home() {
-  const example = useExampleHook("hey");
+const TestComponent = ({ context }: { context: { value: string } }) => {
+
+  return <div>{context.value}</div>;
+};
+
+export default function Home(): JSX.Element {
+
+  const initialState = { value: "initial" };
+  const useContextState = () => initialState;
+
+  const { Provider, useContext } = useContextFactory(
+    initialState,
+    useContextState
+  );
 
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>{example}</main>
-    </div>
+    <Provider>
+      <div className={styles.page}>
+        <main className={styles.main}>
+          <TestComponent context={useContext()} />
+        </main>
+      </div>
+    </Provider>
   );
 }
