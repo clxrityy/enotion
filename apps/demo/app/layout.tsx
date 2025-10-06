f4cf79a7abb2ac1dae65740e588b10687c4b2a7f
clxrityy <clxrityxyz@gmail.com>
2025-10-06 00:29:54 -0400

    ohj

--- apps/demo/app/layout.tsx
+++ apps/demo/app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
 }>): JSX.Element {
   return (
     <html lang="en">
-      <body class={`${geistSans.variable} ${geistMono.variable}`}>
+      <body className={`${geistSans.variable} ${geistMono.variable}`}>
         {children}
       </body>
     </html>
