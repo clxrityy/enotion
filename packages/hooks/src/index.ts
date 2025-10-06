9accd406a4ad69f617b9e6db9b0e9b461d63ca95
clxrityy <clxrityxyz@gmail.com>
2025-10-06 13:05:50 -0400

    exports

--- packages/hooks/src/index.ts
+++ packages/hooks/src/index.ts
@@ -3,3 +3,5 @@ export * from "./useEventListener.js";
 export * from "./useFetch.js";
 export * from "./useLocalStorage.js";
 export * from "./usePreload.js";
+export * from "./useScript.js";
+export * from "./useTheme.js";
