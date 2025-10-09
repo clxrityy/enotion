b8830ca4a1c4c9815e3cef19dfd493cccc662b05
clxrityy <clxrityxyz@gmail.com>
2025-10-09 14:08:54 -0400

    terserOptions

--- packages/@enotion/config/tsup/tsup.config.ts
+++ packages/@enotion/config/tsup/tsup.config.ts
@@ -5,9 +5,21 @@ export default defineConfig([
     entry: {
       index: "src/index.ts",
     },
+    terserOptions: {
+      compress: {
+        defaults: true,
+        drop_console: true,
+        drop_debugger: true,
+      },
+      format: {
+        comments: true,
+        braces: true,
+        preserve_annotations: true,
+      },
+    },
     outDir: "dist",
     format: ["esm"],
-    splitting: false,
+    splitting: true,
     sourcemap: true,
     clean: true,
     minify: "terser",
