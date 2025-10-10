a6faf3a5113dfece0706813c6555d87dc45e5555
clxrityy <clxrityxyz@gmail.com>
2025-10-10 09:12:42 -0400

    button test

---
+++ packages/@enotion/components/tests/Button.test.tsx
@@ -0,0 +1,16 @@
+import { render, screen } from "@testing-library/react";
+import { describe, it, expect, afterEach, jest } from "@jest/globals";
+
+import { Button } from "../src/ButtonSimple";
+
+afterEach(() => {
+  jest.restoreAllMocks();
+});
+
+describe("Button", () => {
+  it("renders with default props", () => {
+    render(<Button>Click me</Button>);
+    const button = screen.getByRole("button", { name: /click me/i });
+    expect(button).toBeDefined();
+  });
+});
