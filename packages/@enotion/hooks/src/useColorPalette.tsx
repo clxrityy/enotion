d3eefd63da8fd5b1e54e1805d5a85d7e756651cd
clxrityy <clxrityxyz@gmail.com>
2025-10-11 22:09:28 -0400

    useColorPalette hook

---
+++ packages/@enotion/hooks/src/useColorPalette.tsx
@@ -0,0 +1,108 @@
+import { type ColorPaletteType, ColorPalettes } from "@enotion/config/constants";
+import { useState } from "react";
+import { createContextFactory } from "./createContextFactory.js";
+
+
+/**
+ * Context for managing the current color palette.
+ * @property palette - The current color palette, which can be one of the predefined palettes or undefined.
+ * @property setPalette - A function to update the current color palette.
+ */
+export interface ColorPaletteContext {
+  palette: ColorPaletteType | undefined;
+  setPalette: (palette: ColorPaletteType) => void;
+}
+
+const initialColorPaletteContext: ColorPaletteContext = {
+  palette: undefined,
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  setPalette: () => { },
+}
+
+/**
+ *
+ */
+const useColorPaletteContext = () => {
+  const [palette, setPalette] = useState<ColorPaletteType | undefined>(initialColorPaletteContext.palette);
+
+  const updatePalette = (newPalette: ColorPaletteType) => {
+    if (ColorPalettes[newPalette]) {
+      setPalette(newPalette);
+    } else {
+      console.warn(`Invalid color palette: ${newPalette}. Falling back to undefined.`);
+      setPalette(undefined);
+    }
+  };
+
+  return {
+    palette,
+    setPalette: updatePalette,
+  };
+}
+
+const { Provider, useContext } = createContextFactory<ColorPaletteContext>(
+  initialColorPaletteContext,
+  useColorPaletteContext,
+);
+
+/**
+ * ColorPaletteProvider - A context provider component for managing color palettes.
+ * @param children - The child components that will have access to the color palette context.
+ * @param context - Optional additional context properties to merge with the default context.
+ *
+ * @description
+ * The `ColorPaletteProvider` component provides a context for managing color palettes in a React application.
+ * It uses the `createContextFactory` utility to create a context with an initial state and a hook for managing the state.
+ * Child components can access and manipulate the color palette using the `useColorPalette` hook.
+ * @example
+ * ```tsx
+ * import { ColorPaletteProvider } from '@enotion/hooks';
+ *
+ * const App = () => (
+ *   <ColorPaletteProvider>
+ *     {children}
+ *   </ColorPaletteProvider>
+ * );
+ * ```
+ */
+export const ColorPaletteProvider = Provider;
+
+/**
+ * useColorPalette - A hook to access the color palette context.
+ * @returns An object containing the current color palette and a function to update it.
+ * @description
+ * The `useColorPalette` hook provides access to the color palette context created by the `ColorPaletteProvider`.
+ * It returns the current color palette and a function to update it, allowing components to easily manage and respond to changes in the color palette.
+ * @example
+ * ```tsx
+ * import { useColorPalette } from '@enotion/hooks';
+ * import { Button, Input } from '@enotion/components';
+ *
+ * const MyComponent = () => {
+ *   const { palette, setPalette } = useColorPalette();
+ *   const [inputValue, setInputValue] = useState(palette || '');
+ *
+ *   return (
+ *     <div>
+ *       Current Palette: {palette}
+ *       <Input
+ *         colorPalette={palette}
+ *         placeholder="Enter color palette"
+ *         value={inputValue} onChange={(e) => {
+ *           setInputValue(e.target.value);
+ *           setPalette(e.target.value as ColorPaletteType);
+ *         }} />
+ *       <Button
+ *         colorPalette={palette} onClick={() => setPalette('dark')}>
+ *         Set Dark Palette
+ *       </Button>
+ *     </div>
+ *   );
+ * };
+ * ```
+ * @see {@link ColorPaletteProvider} - A context provider component for managing color palettes.
+ * @see {@link createContextFactory} - A utility for creating context providers and hooks.
+ * @see {@link ColorPaletteType} - Type representing the available color palettes.
+ * @module useColorPalette
+ */
+export const useColorPalette = useContext;
---
+++ packages/@enotion/hooks/tests/useColorPalette.test.tsx
@@ -0,0 +1,103 @@
+import {
+  jest,
+  describe,
+  beforeEach,
+  afterEach,
+  it,
+  expect,
+} from "@jest/globals";
+import { render, screen } from "@testing-library/react";
+import { useEffect } from "react";
+import { useColorPalette, ColorPaletteProvider } from "../src/useColorPalette";
+import { ColorPaletteType } from "@enotion/config/constants";
+
+describe("useColorPalette", () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  afterEach(() => {
+    jest.restoreAllMocks();
+  });
+
+  it("should have default palette as undefined", () => {
+    const Component = () => {
+      const { palette, setPalette } = useColorPalette();
+      return (
+        <div>
+          <div data-testid="palette">{palette}</div>
+          <div data-testid="setPalette">{typeof setPalette}</div>
+        </div>
+      );
+    };
+
+    render(
+      <ColorPaletteProvider>
+        <Component />
+      </ColorPaletteProvider>,
+    );
+
+    expect(screen.getByTestId("palette").textContent).toBe("");
+    expect(screen.getByTestId("setPalette").textContent).toBe("function");
+  });
+
+  it("should update palette when setPalette is called with a valid palette", () => {
+    const Component = () => {
+      const { palette, setPalette } = useColorPalette();
+
+      // Set palette to 'dark' for testing
+      useEffect(() => {
+        setPalette("dark");
+      }, [setPalette]);
+      return <div data-testid="palette">{palette}</div>;
+    };
+    render(
+      <ColorPaletteProvider>
+        <Component />
+      </ColorPaletteProvider>,
+    );
+
+    expect(screen.getByText("dark")).toBeDefined();
+  });
+
+  it("should not update palette when setPalette is called with an invalid palette", () => {
+    const consoleWarnSpy = jest.spyOn(console, "warn");
+
+    const Component = () => {
+      const { palette, setPalette } = useColorPalette();
+      // Attempt to set an invalid palette for testing
+      useEffect(() => {
+        setPalette("invalid-palette" as ColorPaletteType);
+      }, [setPalette]);
+      return <div data-testid="palette">{palette}</div>;
+    };
+    render(
+      <ColorPaletteProvider>
+        <Component />
+      </ColorPaletteProvider>,
+    );
+    expect(screen.getByTestId("palette").textContent).toBe("");
+    expect(consoleWarnSpy).toHaveBeenCalledWith(
+      "Invalid color palette: invalid-palette. Falling back to undefined.",
+    );
+  });
+
+  it("should update palette when setPalette is called with another valid palette", () => {
+    const Component = () => {
+      const { palette, setPalette } = useColorPalette();
+
+      // Set palette to 'dark' for testing
+      useEffect(() => {
+        setPalette("dark");
+      }, [setPalette]);
+      return <div data-testid="palette">{palette}</div>;
+    };
+    render(
+      <ColorPaletteProvider>
+        <Component />
+      </ColorPaletteProvider>,
+    );
+
+    expect(screen.getByText("dark")).toBeDefined();
+  });
+});
