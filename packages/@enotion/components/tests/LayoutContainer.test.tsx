9fa761b342530a0e3703d3d730548802dd8a0c77
clxrityy <clxrityxyz@gmail.com>
2025-10-16 01:09:13 -0400

    add more tests

--- packages/@enotion/components/tests/LayoutContainer.test.tsx
+++ packages/@enotion/components/tests/LayoutContainer.test.tsx
@@ -91,4 +91,46 @@ describe("LayoutContainer", () => {
     expect(button).toBeDefined();
     expect(button.style.backgroundColor).toBe(buttonBgColor);
   });
+
+  it("does not override existing colorPalette prop on children", () => {
+    const Layout = () => (
+      <LayoutContainer
+        colorPalette="default"
+        renderChildren={() => <Button colorPalette="solarized">Button Child</Button>}
+      />
+    );
+
+    const color = getRGBfromHex(ColorPalettes["solarized"].primary)!;
+    const buttonBgColor = `rgb(${color.r}, ${color.g}, ${color.b})`;
+
+    const { getByText } = render(<Layout />);
+    const button = getByText("Button Child");
+    expect(button).toBeDefined();
+    expect(button.style.backgroundColor).toBe(buttonBgColor);
+  });
+
+  it("handles nested children and fragments", () => {
+    const Layout = () => (
+      <LayoutContainer
+        colorPalette="default"
+        renderChildren={() => (
+          <>
+            <div>Fragment Child 1</div>
+            <div>
+              <Button>Nested Button Child</Button>
+            </div>
+          </>
+        )}
+      />
+    );
+
+    const color = getRGBfromHex(ColorPalettes["default"].primary)!;
+    const buttonBgColor = `rgb(${color.r}, ${color.g}, ${color.b})`;
+
+    const { getByText } = render(<Layout />);
+    expect(getByText("Fragment Child 1")).toBeDefined();
+    const button = getByText("Nested Button Child");
+    expect(button).toBeDefined();
+    expect(button.style.backgroundColor).toBe(buttonBgColor);
+  });
 });
