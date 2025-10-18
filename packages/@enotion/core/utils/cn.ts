ceefb75ca2446a28274b0dacf8f56ad234993304
clxrityy <clxrityxyz@gmail.com>
2025-10-17 20:37:31 -0400

    add doc

--- packages/@enotion/core/utils/cn.ts
+++ packages/@enotion/core/utils/cn.ts
@@ -1,6 +1,18 @@
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 
+/**
+ * Utility function to conditionally join class names and merge Tailwind CSS classes.
+ *
+ * @param inputs - An array of class names or conditional class name objects.
+ * @returns A single string with merged class names.
+ *
+ * @example
+ * <div className={cn("p-4", isActive && "bg-blue-500", "text-white")} />
+ *
+ * @see {@link https://github.com/lukeed/clsx | clsx}
+ * @see {@link https://github.com/dcastil/tailwind-merge | tailwind-merge}
+ */
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
