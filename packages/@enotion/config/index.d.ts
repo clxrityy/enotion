f457a165395521c309a0a969a86cc1b6ab928001
clxrityy <clxrityxyz@gmail.com>
2025-10-20 04:20:28 -0400

    remove unused

--- packages/@enotion/config/index.d.ts
+++ packages/@enotion/config/index.d.ts
@@ -2,7 +2,6 @@ declare module "@enotion/config";
 declare module "@enotion/config/tsup/tsup.config";
 declare module "@enotion/config/tsup/tsup.entry.config";
 declare module "@enotion/config/jest/jest.config";
-declare module "@enotion/config/jest/jest.server.config";
 declare module "@enotion/config/tsconfig/react-library";
 declare module "@enotion/config/tsconfig/nextjs";
 declare module "@enotion/config/tsconfig/base";
