9fb6fe504c2ef4edbc60c8ae076aa198a97d8342
clxrityy <clxrityxyz@gmail.com>
2025-10-20 04:25:01 -0400

    change exports & build

--- packages/@enotion/components/tsup.config.ts
+++ packages/@enotion/components/tsup.config.ts
@@ -1,12 +1,35 @@
-import config from "@enotion/config/tsup/tsup.config";
 import { defineConfig } from "tsup";
 
 export default defineConfig([
   {
-    ...config,
     entry: ["src/index.ts"],
+    outDir: "dist",
+    format: ["esm"],
+    splitting: true,
+    sourcemap: true,
+    clean: true,
+    minify: "terser",
+    tsconfig: "tsconfig.json",
     dts: {
       entry: "src/index.ts",
+    },
+    outExtension({ format }) {
+      return {
+        js: `.js`,
+        dts: `.d.ts`
+      }
+    },
+    terserOptions: {
+      compress: {
+        defaults: true,
+        drop_console: true,
+        drop_debugger: true,
+      },
+      format: {
+        comments: true,
+        braces: true,
+        preserve_annotations: true,
+      },
     },
   },
 ]);
--- packages/@enotion/core/tsup.config.ts
+++ packages/@enotion/core/tsup.config.ts
@@ -1,12 +1,33 @@
-import config from "@enotion/config/tsup/tsup.config";
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-  ...config,
   entry: ["constants/index.ts", "utils/index.ts", "index.ts"],
+  outDir: "dist",
+  format: ["esm"],
+  splitting: true,
+  sourcemap: true,
+  clean: true,
+  minify: "terser",
+  tsconfig: "tsconfig.json",
+  outExtension({ format }) {
+    return {
+      js: `.js`,
+      dts: `.d.ts`
+    }
+  },
   dts: {
     entry: ["constants/index.ts", "utils/index.ts", "index.ts"],
   },
-  clean: true,
-  outDir: "dist",
+  terserOptions: {
+    compress: {
+      defaults: true,
+      drop_console: true,
+      drop_debugger: true,
+    },
+    format: {
+      comments: true,
+      braces: true,
+      preserve_annotations: true,
+    },
+  },
 });
