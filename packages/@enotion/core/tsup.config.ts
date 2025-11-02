9c84f80e9eef59fe62efc3b0eb0dbb01317be9df
clxrityy <clxrityxyz@gmail.com>
2025-11-02 17:53:32 -0500

    export from lib

--- packages/@enotion/core/tsup.config.ts
+++ packages/@enotion/core/tsup.config.ts
@@ -5,4 +5,5 @@ export default defineEntryConfig({
   "constants/index": "constants/index.ts",
   "utils/index": "utils/index.ts",
   "contexts/index": "contexts/index.ts",
+  "lib/index": "lib/index.ts",
 }) as any;
