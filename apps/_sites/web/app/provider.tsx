10b334962ab6ddfb709d05d12d3af723e91804be
clxrityy <clxrityxyz@gmail.com>
2025-11-04 14:41:52 -0500

    add provider

---
+++ apps/_sites/web/app/provider.tsx
@@ -0,0 +1,15 @@
+"use client";
+import { LayoutProvider } from "@enotion/components";
+import { LayoutRenderer } from "@enotion/core";
+import { ReactNode } from "react";
+
+export function Provider({ children }: Readonly<{ children: ReactNode }>) {
+  return (
+    <LayoutProvider>
+      <LayoutRenderer />
+      <div className="absolute w-screen h-screen z-0 *:z-10">
+        {children}
+      </div>
+    </LayoutProvider>
+  );
+}
